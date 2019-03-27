ffd7b11dda53a9f162924faf8773732249a70469
Yevgeny Bulochnik <yevgeny.bulochnik@gmail.com>
2019-03-26 18:59:42 -0700

    Import workbox from local files

--- wp_config/sw.js
+++ wp_config/sw.js
@@ -1,9 +1,5 @@
 workbox.core.skipWaiting();
 workbox.core.clientsClaim();
 
-workbox.precaching.precacheAndRoute([
-  '/',
-  '/login'
-])
 
 workbox.precaching.precacheAndRoute(self.__precacheManifest);
